5ed297c43b3d6bda37ae8b78971e3384de2a8563
Kacper <kacper.gajko1@gmail.com>
2024-08-19 14:47:23 +0200

    Initial commit

---
+++ build.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .macos },
+    .{ .cpu_arch = .x86_64, .os_tag = .linux },
+    .{ .cpu_arch = .x86_64, .os_tag = .windows },
+};
+
+pub fn build(b: *std.Build) !void {
+    for (targets) |t| {
+        const exe = b.addExecutable(.{
+            .name = "hello",
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(t),
+            .optimize = .ReleaseSafe,
+        });
+
+        const target_output = b.addInstallArtifact(exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try t.zigTriple(b.allocator),
+                },
+            },
+        });
+
+        b.getInstallStep().dependOn(&target_output.step);
+    }
+}
