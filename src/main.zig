34d4c8809e52edcb6a6378b260d4b218738e1376
Kacper <kacper.gajko1@gmail.com>
2024-08-20 19:58:42 +0200

    Drawing square on screen

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,41 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-pub fn main() !void {
-    const screenWidth = 1920;
-    const screenHeight = 1080;
+const Vector2 = rl.Vector2;
 
-    rl.initWindow(screenWidth, screenHeight, "TEST");
+const Enemy = struct {
+    health: u8,
+    position: Vector2,
+    size: Vector2,
+};
+
+const screenWidth = 1280;
+const screenHeight = 720;
+
+var enemy: Enemy = undefined;
+
+pub fn StartGame() !void {
+    enemy.position.x = screenWidth / 2;
+    enemy.position.y = screenHeight / 2;
+
+    enemy.size.x = 10;
+    enemy.size.y = 10;
+}
+
+pub fn main() !void {
+    rl.initWindow(screenWidth, screenHeight, "Ziggers");
 
     rl.setTargetFPS(60);
 
+    try StartGame();
+
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
+
+        rl.clearBackground(rl.Color.black);
+
+        rl.drawRectangleV(enemy.position, enemy.size, rl.Color.white);
+
         defer rl.endDrawing();
-
-        rl.clearBackground(rl.Color.white);
-
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
     }
 }
