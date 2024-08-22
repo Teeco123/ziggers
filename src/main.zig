877d5c40bd9f69ccb4b19df6dd8131ff858c0ad6
Kacper <kacper.gajko1@gmail.com>
2024-08-22 19:47:42 +0200

    Moving enemy through map

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,19 @@ const std = @import("std");
 const rl = @import("raylib");
 
 const Vector2 = rl.Vector2;
+const math = std.math;
 
 const Map = struct {
     positions: u8,
     cords: [10]Vector2,
+    point: u8,
 };
 
 const Enemy = struct {
     health: u8,
     position: Vector2,
-    size: Vector2,
-    speed: u8,
+    size: f32,
+    speed: f32,
 };
 
 const screenWidth = 1280;
@@ -25,32 +27,53 @@ pub fn StartGame() !void {
     rl.initWindow(screenWidth, screenHeight, "Ziggers");
     rl.setTargetFPS(60);
 
-    enemy.position = Vector2.init(screenWidth / 2, screenHeight / 2);
-    enemy.size = Vector2.init(15, 15);
-
     map.positions = 5;
+    map.point = 0;
     map.cords[0] = Vector2.init(0, 300);
     map.cords[1] = Vector2.init(400, 300);
     map.cords[2] = Vector2.init(400, 500);
     map.cords[3] = Vector2.init(800, 500);
     map.cords[4] = Vector2.init(800, 150);
-    map.cords[5] = Vector2.init(1270, 150);
+    map.cords[5] = Vector2.init(1280, 150);
+
+    enemy.position = Vector2.init(map.cords[0].x, map.cords[0].y);
+    enemy.speed = 0.6;
+    enemy.size = 10;
+}
+
+pub fn Update() !void {
+    while (map.point <= map.positions) {
+        enemy.position = enemy.position.moveTowards(map.cords[map.point], enemy.speed);
+
+        if (rl.Vector2.equals(enemy.position, map.cords[map.point]) == 1) {
+            map.point += 1;
+        }
+        break;
+    }
+}
+
+pub fn Draw() !void {
+    rl.beginDrawing();
+
+    rl.clearBackground(rl.Color.black);
+
+    var drawPoint: u8 = 0;
+    while (drawPoint < map.positions) {
+        rl.drawLineV(map.cords[drawPoint], map.cords[drawPoint + 1], rl.Color.pink);
+        drawPoint += 1;
+    }
+
+    rl.drawPoly(enemy.position, 8, enemy.size, 0, rl.Color.blue);
+
+    defer rl.endDrawing();
 }
 
 pub fn main() !void {
     try StartGame();
 
     while (!rl.windowShouldClose()) {
-        rl.beginDrawing();
-
-        rl.clearBackground(rl.Color.black);
-
-        var i: u8 = 0;
-        while (i < map.positions) {
-            rl.drawLineV(map.cords[i], map.cords[i + 1], rl.Color.pink);
-            i += 1;
-        }
-
-        defer rl.endDrawing();
+        try Update();
+        try Draw();
     }
+    rl.closeWindow();
 }
