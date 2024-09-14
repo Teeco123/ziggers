e5c4b11505b377cc459d43aa73534dca69366377
Teeco123 <kacper.gajko1@icloud.com>
2024-09-14 15:05:19 +0200

    Better zig build

--- build.zig
+++ build.zig
@@ -1,41 +1,30 @@
 const std = @import("std");
 
-const targets: []const std.Target.Query = &.{
-    .{ .cpu_arch = .x86_64, .os_tag = .linux },
-    .{ .cpu_arch = .x86_64, .os_tag = .windows },
-    .{ .cpu_arch = .aarch64, .os_tag = .windows },
-    .{ .cpu_arch = .x86_64, .os_tag = .macos },
-    .{ .cpu_arch = .aarch64, .os_tag = .macos },
-};
-
 pub fn build(b: *std.Build) !void {
-    for (targets) |t| {
-        const raylib_dep = b.dependency("raylib-zig", .{
-            .target = b.host,
-            .optimize = .ReleaseSafe,
-        });
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-        const raylib = raylib_dep.module("raylib"); // main raylib module
-        const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
-        const exe = b.addExecutable(.{
-            .name = "Ziggers",
-            .root_source_file = b.path("src/main.zig"),
-            .target = b.host,
-            .optimize = .ReleaseSafe,
-        });
+    const raylib = raylib_dep.module("raylib"); // main raylib module
+    const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
 
-        exe.linkLibrary(raylib_artifact);
-        exe.root_module.addImport("raylib", raylib);
+    const exe = b.addExecutable(.{
+        .name = "Ziggers",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-        const target_output = b.addInstallArtifact(exe, .{
-            .dest_dir = .{
-                .override = .{
-                    .custom = try t.zigTriple(b.allocator),
-                },
-            },
-        });
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
 
-        b.getInstallStep().dependOn(&target_output.step);
-    }
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run Ziggers");
+    run_step.dependOn(&run_cmd.step);
+
+    b.installArtifact(exe);
 }
